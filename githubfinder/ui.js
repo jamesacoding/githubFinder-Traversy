4cb228abbe6f9ab74ad2692d0fc458fa597f4d4d
james a <jamesacoding@gmail.com>
2022-02-03 11:29:39 -0500

    Practice Init

--- githubfinder/ui.js
+++ githubfinder/ui.js
@@ -30,4 +30,37 @@ class UI {
       <div id="repos"></div>
     `;
     }
+
+      clearProfile() {
+        this.profile.innerHTML = '';
+      }
+
+      showAlert(message, className) {
+
+        this.clearAlert();
+        const div = document.createElement('div');
+        div.className = className;
+        div.appendChild(document.createTextNode(message));
+
+        const container = document.querySelector('.search-container');
+        
+
+        const search = document.querySelector('.search');
+
+        container.insertBefore(div, search);
+
+        setTimeout(() => {
+          this.clearAlert();
+          }, 3000)
+
+      }
+
+      clearAlert() {
+        const currentAlert = document.querySelector('.alert');
+
+        if(currentAlert) {
+          currentAlert.remove();
+        }
+      }
+
 }
